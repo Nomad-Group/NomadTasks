720a3b4b76cbf91418b71c87931ff6fc4eb820cb
Jan Schmitter <jan.schmitter@gmail.com>
2021-03-08 11:10:17 +0100

    Fixed threadjob sometimes not executing jobs

--- code/threadjob/include/threadjob/Manager.h
+++ code/threadjob/include/threadjob/Manager.h
@@ -24,62 +24,30 @@ namespace nmd::job
 	  private:
 		std::recursive_mutex _executionMutex;
 
-		std::vector<IJob *> _jobs = {};
-		std::vector<IJob *> _nextJobs = {};
+		std::vector<IJob *> _jobs;
 
 	  public:
 		JobSystem() = default;
 		virtual ~JobSystem() = default;
 		static std::unique_ptr<JobSystem> Create();
 
-		// If you want to run more jobs, change the default val in the func header
-		void Run(uint32_t jobsToExecute = 250)
+		void Run()
 		{
-			uint32_t jobsExecuted = 0;
-
-			std::vector<IJob *> runnedJobs;
+			std::vector<IJob *> jobsToRun;
 
 			{
 				std::lock_guard<std::recursive_mutex> lk{ _executionMutex };
-				_jobs = _nextJobs;
+				std::swap(jobsToRun, _jobs);
 			}
 
-			auto it = _jobs.begin();
-
-			// Run jobs and remove them from list
-			for (uint32_t n = 0; n < jobsToExecute; ++n) {
-				if (_jobs.empty() || it == _jobs.end()) {
-					break;
-				}
-				
-				auto fn = *it;
-
-				if ((*fn)(this)) {
-					delete fn;
-					runnedJobs.push_back(fn);
-					it = _jobs.erase(it);
-				} else {
-					++it;
-				}
-
-				++jobsToExecute;
+			for (auto job : jobsToRun) {
+				job->Execute();
 			}
-
-			// Move jobs to other vectors for next run and clear jobs queue
-			{
-				std::lock_guard<std::recursive_mutex> lk{ _executionMutex };
-				for (auto &it : runnedJobs) {
-					_nextJobs.erase(std::remove(_nextJobs.begin(), _nextJobs.end(), it), _nextJobs.end());
-				}
-			}
-
-			_jobs.clear();
 		}
 
 		void Reset()
 		{
 			_jobs.clear();
-			_nextJobs.clear();
 		}
 
 		void Add(IJob *job)
@@ -142,14 +110,14 @@ namespace nmd::job
 		{
 			std::lock_guard<std::recursive_mutex> lk{ _executionMutex };
 			auto n = new Job<T>(fn);
-			_nextJobs.emplace_back(n);
+			_jobs.emplace_back(n);
 			return *n;
 		}
 
 		void AddInternal(IJob *fn)
 		{
 			std::lock_guard<std::recursive_mutex> lk{ _executionMutex };
-			_nextJobs.push_back(fn);
+			_jobs.push_back(fn);
 		}
 	};
 } // namespace nmd::job
